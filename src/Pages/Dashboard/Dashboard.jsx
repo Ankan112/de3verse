181d603103eaad00499e799c957d4b96bd778c6d
Ankan112 <ankanhalder112@gmail.com>
2023-04-10 21:23:45 +0600

    footer complete

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Footer from '../../Components/Footer/Footer';
+import SignUp from '../../Components/SingUp/SignUp';
 
 const Dashboard = () => {
     return (
         <div>
+            <SignUp></SignUp>
             <Footer></Footer>
         </div>
     );
