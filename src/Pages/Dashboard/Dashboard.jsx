eb32d291dfab6ae46b660683f69f4afdcdc944ad
Ankan112 <ankanhalder112@gmail.com>
2023-04-10 22:24:21 +0600

    partners section finished

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Footer from '../../Components/Footer/Footer';
 import SignUp from '../../Components/SingUp/SignUp';
+import Partners from '../../Components/Partners/Partners';
 
 const Dashboard = () => {
     return (
         <div>
+            <Partners></Partners>
             <SignUp></SignUp>
             <Footer></Footer>
         </div>
