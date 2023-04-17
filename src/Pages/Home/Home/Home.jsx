705f114dfdc50b217a3943bfa81184d4ff973fe9
Ankan112 <ankanhalder112@gmail.com>
2023-04-17 14:23:59 +0600

    font change in homepage

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            <h2 className='text-red-500'>This is  home page</h2>
+            <h2 className='text-white font-akira text-center text-2xl '>This is  home page</h2>
         </div>
     );
 };
